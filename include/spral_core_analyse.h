54fe87d4286874b828288fab549cc2a99761adef
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2015-12-20 15:15:47 +0000

    Fix C++ compat
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/incachesolver@524 33810a50-c353-450f-be6c-41f7241c649d

--- include/spral_core_analyse.h
+++ include/spral_core_analyse.h
@@ -1,8 +1,16 @@
 #ifndef SPRAL_BASIC_ANALYSE_H
 #define SPRAL_BASIC_ANALYSE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int spral_core_analyse_basic_analyse(int n, const int ptr[], const int row[],
       int perm[], int *nnodes, int **sptr, int **sparent, long **rptr,
       int **rlist, int nemin, long *nfact, long *nflops, int base);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* SPRAL_BASIC_ANALYSE_H */
--- include/spral_metis_wrapper.h
+++ include/spral_metis_wrapper.h
@@ -1,7 +1,15 @@
 #ifndef SPRAL_METIS_WRAPPER_H
 #define SPRAL_METIS_WRAPPER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Order a matrix using METIS */
 int spral_metis_order(int n, const int ptr[], const int row[], int perm[], int invp[], int base);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* SPRAL_METIS_WRAPPER_H */
