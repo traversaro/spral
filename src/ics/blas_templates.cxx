5392d4483966dd49686dffce2e74a1a386e4b1b7
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2015-12-23 21:31:53 +0000

    Split fully specifed defns from declarations to prevent multiple defn
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/incachesolver@535 33810a50-c353-450f-be6c-41f7241c649d

---
+++ src/ics/blas_templates.cxx
@@ -0,0 +1,49 @@
+#include "blas_templates.hxx"
+
+namespace spral {
+namespace ics {
+
+/***************************************************************************
+ * BLAS
+ ***************************************************************************/
+
+/* SYRK */
+extern "C" {
+   void dsyrk_(char const* uplo, char const* trans, int const* n, int const* k,
+         double const* alpha, double const* a, int const* lda,
+         double const* beta, double* c, int const* ldc);
+}
+template<> void syrk<double>(char uplo, char trans, int n, int k, double alpha,
+      double const* a, int lda, double beta, double* c, int ldc) {
+   dsyrk_(&uplo, &trans, &n, &k, &alpha, a, &lda, &beta, c, &ldc);
+}
+
+/* TRSM */
+extern "C" {
+   void dtrsm_(char const* side, char const* uplo, char const* trans,
+         char const *diag, int const* m, int const* n, double const* alpha,
+         double const* a, int const* lda, double* b, int const* ldb);
+}
+template<> void trsm<double>(char side, char uplo, char trans, char diag,
+      int m, int n, double alpha, double const* a, int lda, double* b,
+      int ldb) {
+   dtrsm_(&side, &uplo, &trans, &diag, &m, &n, &alpha, a, &lda, b, &ldb);
+}
+
+/***************************************************************************
+ * LAPACK
+ ***************************************************************************/
+
+/* POTRF */
+extern "C" {
+   void dpotrf_(char const* uplo, int const* n, double* a, int const* lda,
+         int *info);
+}
+template<> int potrf<double> (char uplo, int n, double* a, int lda) {
+   int info;
+   dpotrf_(&uplo, &n, a, &lda, &info);
+   return info;
+}
+
+}
+} /* spral */
--- src/ics/blas_templates.hxx
+++ src/ics/blas_templates.hxx
@@ -1,19 +1,5 @@
 #pragma once
 
-extern "C" {
-   /* BLAS */
-   void dsyrk_(char const* uplo, char const* trans, int const* n, int const* k,
-         double const* alpha, double const* a, int const* lda,
-         double const* beta, double* c, int const* ldc);
-   void dtrsm_(char const* side, char const* uplo, char const* trans,
-         char const *diag, int const* m, int const* n, double const* alpha,
-         double const* a, int const* lda, double* b, int const* ldb);
-
-   /* LAPACK */
-   void dpotrf_(char const* uplo, int const* n, double* a, int const* lda,
-         int *info);
-}
-
 namespace spral {
 namespace ics {
 
@@ -22,32 +8,19 @@ namespace ics {
  ***************************************************************************/
 
 /* SYRK */
-template <typename T> void syrk(char uplo, char trans, int n, int k, T alpha,
+template<typename T> void syrk(char uplo, char trans, int n, int k, T alpha,
       T const* a, int lda, T beta, T* c, int ldc);
-template <> void syrk<double>(char uplo, char trans, int n, int k, T alpha,
-      T const* a, int lda, T beta, T* c, int ldc) {
-   dsyrk_(&uplo, &trans, &n, &k, &alpha, a, &lda, &beta, c, &ldc);
-}
 
 /* TRSM */
-template <typename T> void trsm(char side, char uplo, char trans, char diag,
+template<typename T> void trsm(char side, char uplo, char trans, char diag,
       int m, int n, T alpha, T const* a, int lda, T* b, int ldb);
-template <> void trsm<double>(char side, char uplo, char trans, char diag,
-      int m, int n, T alpha, T const* a, int lda, double* b, int ldb) {
-   dtrsm_(&side, &uplo, &trans, &diag, &m, &n, &alpha, a, &lda, b, &ldb);
-}
 
 /***************************************************************************
  * LAPACK
  ***************************************************************************/
 
 /* POTRF */
-template <typename T> int potrf(char uplo, int n, T* a, int lda);
-template <> int potrf<double> (char uplo, int n, double* a, int lda) {
-   int info;
-   dpotrf_(&uplo, &n, a, &lda, &info);
-   return info;
-}
+template<typename T> int potrf(char uplo, int n, T* a, int lda);
 
 }
 } /* spral */
