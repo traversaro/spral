ba64c96f98fb880297520c8fc2955cfa2d6d701d
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2015-12-28 12:43:48 +0000

    Fix handling o null generated element
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/incachesolver@544 33810a50-c353-450f-be6c-41f7241c649d

--- src/ics/Node.hxx
+++ src/ics/Node.hxx
@@ -28,9 +28,6 @@ public:
       /* Setup working pointers */
       T *ldiag = &lval[loffset_];
       T *lrect = &lval[loffset_ + n_];
-      int ldcontrib = m_ - n_;
-      T *contrib = memhandler.get<T>((m_-n_)*((long) ldcontrib));
-      int *map = memhandler.get<int>(node_.get_max_tree_row_index());
 
       /* Add A */
       for(auto ait=node_.a_to_l_begin(); ait!=node_.a_to_l_end(); ++ait) {
@@ -42,27 +39,36 @@ public:
       int info = potrf<T>('L', n_, ldiag, ldl_);
       if(info) throw NotPositiveDefiniteException(info);
 
-      /* Apply to block below diagonal */
-      trsm<T>('R', 'L', 'T', 'N', m_-n_, n_, 1.0, ldiag, ldl_, lrect, ldl_);
+      /* Only work with generated element if it exists! */
+      if(m_ - n_ > 0) { 
+         /* Get workspace */
+         int ldcontrib = m_ - n_;
+         T *contrib = memhandler.get<T>((m_-n_)*((long) ldcontrib));
+         int *map = memhandler.get<int>(node_.get_max_tree_row_index());
 
-      /* Form generated element into workspace */
-      syrk<T>('L', 'N', m_-n_, n_, -1.0, lrect, ldl_, 0.0, contrib, ldcontrib);
+         /* Apply to block below diagonal */
+         trsm<T>('R', 'L', 'T', 'N', m_-n_, n_, 1.0, ldiag, ldl_, lrect, ldl_);
 
-      /* Distribute elements of generated element to ancestors */
-      auto row_start = std::next(node_.row_begin(), n_);
-      const T* contrib_ptr = contrib;
-      for(auto anc_itr = anc_begin; anc_itr != anc_end; ++anc_itr) {
-         int used_cols = anc_itr->add_contribution(
-               lval, row_start, node_.row_end(),
-               contrib_ptr, ldcontrib, map
-               );
-         std::advance(row_start, used_cols);
-         contrib_ptr += used_cols*(ldcontrib+1); // Advance to diagonal entry
+         /* Form generated element into workspace */
+         syrk<T>('L', 'N', m_-n_, n_, -1.0, lrect, ldl_, 0.0, contrib,
+               ldcontrib);
+
+         /* Distribute elements of generated element to ancestors */
+         auto row_start = std::next(node_.row_begin(), n_);
+         const T* contrib_ptr = contrib;
+         for(auto anc_itr = anc_begin; anc_itr != anc_end; ++anc_itr) {
+            int used_cols = anc_itr->add_contribution(
+                  lval, row_start, node_.row_end(),
+                  contrib_ptr, ldcontrib, map
+                  );
+            std::advance(row_start, used_cols);
+            contrib_ptr += used_cols*(ldcontrib+1); // Advance to diagonal entry
+         }
+
+         /* Release workspace */
+         memhandler.release<int>(map, node_.get_max_tree_row_index());
+         memhandler.release<T>(contrib, (m_-n_)*((long) ldcontrib));
       }
-
-      /* Release workspace */
-      memhandler.release<T>(contrib, (m_-n_)*((long) ldcontrib));
-      memhandler.release<int>(map, node_.get_max_tree_row_index());
    }
 
    /** Adds the contribution in contrib as per list (of rows).
