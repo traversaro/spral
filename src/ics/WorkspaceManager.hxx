1a754c2766597b9532c6f768bfdcd8a0dcc732de
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2015-12-28 14:24:24 +0000

    Fix bug and compliance in WorkspaceManager
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/incachesolver@550 33810a50-c353-450f-be6c-41f7241c649d

--- src/ics/WorkspaceManager.hxx
+++ src/ics/WorkspaceManager.hxx
@@ -8,13 +8,13 @@ namespace ics {
 class WorkspaceManager {
 public:
    WorkspaceManager(size_t maxsz)
-   : workspace(nullptr), head(0), maxsz_(maxsz)
+   : workspace_(nullptr), head_(0), maxsz_(maxsz)
    {
-      workspace = malloc(maxsz);
-      if(!workspace) throw std::bad_alloc();
+      workspace_ = malloc(maxsz);
+      if(!workspace_) throw std::bad_alloc();
    }
    ~WorkspaceManager() {
-      if(workspace) free(workspace);
+      if(workspace_) free(workspace_);
    }
 
    /** Returns an uninitialized workspace of size num*sizeof(T).
@@ -22,21 +22,21 @@ public:
     */
    template<typename T>
    T *get(size_t num) {
-      T *ptr = (T *) ( ((char *) workspace) + head );
-      head += num*sizeof(T);
+      T *ptr = (T *) ( ((char *) workspace_) + head_ );
+      head_ += num*sizeof(T);
       return ptr;
    }
 
    template<typename T>
    void release(T const* ptr, size_t num) {
-      head -= num*sizeof(num);
-      if(ptr != (T *) ( ((char *) workspace) + head ))
+      head_ -= num*sizeof(T);
+      if(ptr != (T *) ( ((char *) workspace_) + head_ ))
          throw std::runtime_error("Memory released in incorrect order!");
    }
 
 private:
-   void *workspace;
-   size_t head; // current pointer to next allocation
+   void *workspace_;
+   size_t head_; // current pointer to next allocation
    size_t maxsz_; // Maximum size (in bytes)
 };
 
